7e54aa9c1b05717032de7504abb158b72b21c452
LAPTOP-5SS0CNUL\Admin <hoangkhangtruong1705@gmail.com>
2025-05-23 10:47:23 +0700

    5th-commit

--- src/util/config.js
+++ src/util/config.js
@@ -1,6 +1,6 @@
 
 
-export const DOMAIN = 'dpg-d0i4meumcj7s739ms2e0-a.singapore-postgres.render.com';
+export const DOMAIN = 'https://be-shopee-project.onrender.com';
 // export const DOMAIN = 'http://localhost:8000';
 export const TOKEN = ''
 export const tokenBackEnd  = ''
